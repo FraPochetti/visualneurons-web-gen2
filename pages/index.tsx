3d9f3af0da59bb93eb59decd9712d25aa63a83bb
Ubuntu <ubuntu@ip-172-31-17-151.eu-west-1.compute.internal>
2025-02-09 18:01:17 +0000

    Image upload seems to worknpm run devnpm run dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { uploadData, list, getUrl } from "aws-amplify/storage";
 import { getCurrentUser } from "aws-amplify/auth";
+import { fetchAuthSession } from "aws-amplify/auth";
 
 export default function PhotoUpload() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -18,12 +19,18 @@ export default function PhotoUpload() {
     if (!selectedFile) return;
 
     try {
-      // Get the user's identity ID
-      const user = await getCurrentUser();
-      console.log("User object:", user); // Debugging
-      console.log("User identityId:", user.userId); // Debugging
+      // const user = await getCurrentUser();
+      // console.log("User object:", user); 
+      // console.log("User identityId:", user.userId); 
 
-      const path = `photos/${user.userId}/${selectedFile.name}`;
+      // const path = `photos/${user.userId}/${selectedFile.name}`;
+      // console.log("Uploading to:", path); 
+      const session = await fetchAuthSession();
+      const identityId = session.identityId;
+
+      console.log("Cognito Identity ID:", identityId); // Debugging
+
+      const path = `photos/${identityId}/${selectedFile.name}`;
       console.log("Uploading to:", path); // Debugging
 
       await uploadData({
