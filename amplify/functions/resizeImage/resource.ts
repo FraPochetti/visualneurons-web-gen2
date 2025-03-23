d36a0f4d4ed89ffb6de0a26d9eeb21f8582b993d
FraPochetti <fra.pochetti@gmail.com>
2025-03-23 22:11:25 +0000

    Add Lambda Layer

--- amplify/functions/resizeImage/resource.ts
+++ amplify/functions/resizeImage/resource.ts
@@ -4,4 +4,7 @@ export const resizeImage = defineFunction({
     name: 'resizeImage',
     entry: './handler.ts',
     timeoutSeconds: 30,
+    layers: {
+        nodeSharp: "arn:aws:lambda:eu-central-1:257446244580:layer:nodeSharp:1",
+    },
 });
