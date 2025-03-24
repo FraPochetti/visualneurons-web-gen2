a0544a7fcd9855fa665ccb61f908ea8b9531e215
FraPochetti <fra.pochetti@gmail.com>
2025-03-24 00:02:45 +0000

    Switch to Python

--- amplify/functions/resizeImage/resource.ts
+++ amplify/functions/resizeImage/resource.ts
@@ -1,5 +1,5 @@
 import { defineFunction } from "@aws-amplify/backend";
-import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { Function, Runtime, Code, LayerVersion } from "aws-cdk-lib/aws-lambda";
 import { Duration } from "aws-cdk-lib";
 import * as path from "path";
 import { fileURLToPath } from 'url';
@@ -9,8 +9,15 @@ const functionDir = path.dirname(fileURLToPath(import.meta.url));
 export const resizeImage = defineFunction((scope) =>
     new Function(scope, "resizeImage", {
         handler: "handler.handler", // refers to handler.py, function "handler"
-        runtime: Runtime.PYTHON_3_9, // using the enum from aws-cdk-lib/aws-lambda
+        runtime: Runtime.PYTHON_3_10, // updated to Python 3.10
         code: Code.fromAsset(path.join(functionDir, "./")), // all files in this folder
         timeout: Duration.seconds(30),
+        layers: [
+            LayerVersion.fromLayerVersionArn(
+                scope,
+                "PillowLayer",
+                "arn:aws:lambda:eu-central-1:770693421928:layer:Klayers-p310-Pillow:10"
+            )
+        ]
     })
 );
